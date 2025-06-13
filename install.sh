fb1e05ae2a421749073354f34fc64868670d0066
hscript06 <pablotejerinagp@gmail.com>
2025-06-13 13:07:00 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo "🔧 Instalando scanpt..."
+if [ "$EUID" -ne 0 ]; then
+    echo "❌ Por favor, ejecuta este script como root (usa sudo)"
+    exit 1
+fi
+SOURCE="./scanpt"
+DEST="/usr/local/bin/scanpt"
+cp "$SOURCE" "$DEST"
+chmod +x "$DEST"
+echo "✅ Instalación completada. Ya puedes usar el comando: scanpt"
